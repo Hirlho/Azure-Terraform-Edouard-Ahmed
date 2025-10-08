ec33abe3f34693fd66690fde98fe2d93194d002b
estamboulian <estamboulian@myges.fr>
2025-10-08 15:18:29 +0200

    add deploymeny nginx

--- nsg.tf
+++ nsg.tf
@@ -15,6 +15,18 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "K3s-NodePort"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["30000-35000"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   depends_on = [azurerm_resource_group.rg]
 }
 
